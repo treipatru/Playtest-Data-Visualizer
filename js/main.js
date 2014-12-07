cba22a96611af2c481c49b97046c0edf78c5089b
Andrei34 <andrei@planet34.org>
2014-12-07 11:51:55 +0400

    More Charts
    > Added more charts.
    > Added export plugin to page load so charts can now be saved.

--- js/main.js
+++ js/main.js
@@ -8,22 +8,22 @@ var completeDataLoaded = false;
 ///////////////////////////////////////////////////////////////////////////////
 
 $(function() {
-	$( "#datePickStart" ).datepicker({
-		minDate: new Date(2014, 9 - 1),
-		maxDate: new Date(2015, 4 - 1),
-		dateFormat: 'yy-mm-dd'});
+    $( "#datePickStart" ).datepicker({
+        minDate: new Date(2014, 9 - 1),
+        maxDate: new Date(2015, 4 - 1),
+        dateFormat: 'yy-mm-dd'});
 });
 
 $(function() {
-	$( "#datePickEnd" ).datepicker({
-		minDate: new Date(2014, 9 - 1),
-		maxDate: new Date(2015, 4 - 1),
-		dateFormat: 'yy-mm-dd'});
+    $( "#datePickEnd" ).datepicker({
+        minDate: new Date(2014, 9 - 1),
+        maxDate: new Date(2015, 4 - 1),
+        dateFormat: 'yy-mm-dd'});
 });
 
 function loadDateValues () {
-	oStartDate = $( "#datePickStart" ).datepicker( "getDate" );
-	oEndDate = $( "#datePickEnd" ).datepicker( "getDate" );
+    oStartDate = $( "#datePickStart" ).datepicker( "getDate" );
+    oEndDate = $( "#datePickEnd" ).datepicker( "getDate" );
 }
 
 $(function(){
@@ -31,19 +31,19 @@ $(function(){
    loadDateValues();
 
    if (oStartDate === null || oEndDate === null) {
-   	$("#debug").text("You must select two dates to validate.");
-   	return;
+    $("#debug").text("You must select two dates to validate.");
+    return;
    } else {
-   	//Go through the dates between the START and END create an array with all dates
-   	var oCurrentDate = new Date(oStartDate);
-    var aDatesList = [];	
+    //Go through the dates between the START and END create an array with all dates
+    var oCurrentDate = new Date(oStartDate);
+    var aDatesList = [];    
 
     while (oCurrentDate <= oEndDate) {
         aDatesList.push(new Date(oCurrentDate));
         oCurrentDate.setDate(oCurrentDate.getDate() + 1);
     }
     for (i = 0; i < aDatesList.length; i++) {
-    	aDatesList[i] = $.datepicker.formatDate ("yy-mm-dd", aDatesList[i]);
+        aDatesList[i] = $.datepicker.formatDate ("yy-mm-dd", aDatesList[i]);
     }
 
     //Load all data files from the server into an array. Filelist is generated server-side.
@@ -141,30 +141,7 @@ $(function(){
             enabled: false
         },
         xAxis: { //List of all loaded FILES
-            categories: [
-                'Game01',
-                'Game02',
-                'Game03',
-                'Game04',
-                'Game05',
-                'Game06',
-                'Game07',
-                'Game08',
-                'Game09',
-                'Game10',
-                'Game11',
-                'Game12',
-                'Game13',
-                'Game14',
-                'Game15',
-                'Game16',
-                'Game17',
-                'Game18',
-                'Game19',
-                'Game20',
-                'Game21',
-                'Game22',
-            ]
+            categories: ['Game01','Game02','Game03','Game04','Game05','Game06','Game07','Game08','Game09','Game10','Game11','Game12','Game13','Game14','Game15','Game16','Game17','Game18','Game19','Game20','Game21','Game22']
         },
         yAxis: {
             min: 0,
@@ -204,7 +181,7 @@ $(function(){
             text: 'Waves Versus Souls Collection'
         },
         legend: {
-            enabled: false
+            enabled: true
         },
         xAxis: {
             title: {
@@ -246,19 +223,19 @@ $(function(){
         },
         series: [{
             name: 'Game01',//FILENAME(Data Object)
-            color: 'rgba(223, 83, 83, .5)',
+            // color: 'rgba(223, 83, 83, .5)',
             data: [[1, 10],[2,30],[3, 60], [4, 70], [5,100]] //1st digit is wave number, 2nd digit is souls collected
           }, {
             name: 'Game02',
-            color: 'rgba(223, 83, 200, .5)',
+            // color: 'rgba(223, 83, 200, .5)',
             data: [[1, 4],[2,45],[3, 50], [4, 80], [5,120]]
           }, {
             name: 'Game03',
-            color: 'rgba(2, 83, 83, .5)',
+            // color: 'rgba(2, 83, 83, .5)',
             data: [[1, 7],[2,38],[3, 20], [4, 85], [5,110]]
           }, {
             name: 'Game04',
-            color: 'rgba(223, 250, 83, .5)',
+            // color: 'rgba(223, 250, 83, .8)',
             data: [[1, 8],[2,49],[3, 42], [4, 5], [5,105]]
           }
           ]
@@ -311,49 +288,99 @@ $(function(){
     });
 });
    //Display Chart #4
-$(function () {
-    $('#chartAttackTypesPerWave').highcharts({
+    $(function () {
+        $('#chartAttackTypesPerWave').highcharts({
+            chart: {
+                type: 'area'
+            },
+            title: {
+                text: 'Attacks Distribution'
+            },
+            subtitle: {
+                text: 'Averaged from all selected play sessions'
+            },
+            xAxis: {
+                categories: ['W01','W02','W03','W04','W05','W06','W07','W08','W09','W10','W11','W12','W13','W14','W15','W16','W17','W18','W19','W20','W21','W22','W23','W24','W25','W26','W27','W28','W29','W30'],
+                tickmarkPlacement: 'on',
+                title: {
+                    enabled: false
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Percent'
+                }
+            },
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.percentage:.0f}%</b> ({point.y:,.0f} hits)<br/>',
+                shared: true
+            },
+            plotOptions: {
+                area: {
+                    stacking: 'percent',
+                    lineColor: '#ffffff',
+                    lineWidth: 1,
+                    marker: {
+                        lineWidth: 1,
+                        lineColor: '#ffffff'
+                    }
+                }
+            },
+            series: [{//Each number is an average calculated per wave from all data sources
+                name: 'Melee',
+                data: [178,177,156,134,173,122,138,157,172,164,171,157,107,187,185,166,147,171,122,147,141,131,176,137,100,139,144,234,100,168]
+            }, {
+                name: 'Charged Melee',
+                data: [0,0,0,0,0,0,34,27,34,34,17,27,33,39,31,34,31,37,32,29,20,30,26,32,26,36,26,36,25,27]
+            }, {
+                name: 'Range',
+                data: [58,89,84,52,94,132,113,78,78,67,89,78,133,114,120,71,82,67,79,69,78,125,118,102,87,131,76,73,129,68]
+            }, {
+                name: 'Charged Range',
+                data: [0,0,0,0,0,0,0,0,0,56,49,45,68,57,42,56,61,53,52,59,54,53,49,62,47,55,60,60,65,66]
+            }]
+        });
+    });
+
+   //Display Chart #5
+   $(function () {
+    $('#chartWalkingStrafing').highcharts({
         chart: {
-            type: 'line'
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
         },
         title: {
-            text: 'Attacks Used'
+            text: 'In-Game Movement'
         },
-        subtitle: {
-            text: 'Averaged from all selected play sessions'
-        },
-        xAxis: {//Total Number of Waves
-            categories: ['W01','W02','W03','W04','W05','W06','W07','W08','W09','W10','W11','W12','W13','W14','W15','W16','W17','W18','W19','W20','W21','W22','W23','W24','W25','W26','W27','W28','W29','W30']
-        },
-        yAxis: {
-            title: {
-                text: 'No Of Uses'
-            }
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
         },
         plotOptions: {
-            line: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
                 dataLabels: {
-                    enabled: true
-                },
-                enableMouseTracking: false
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
             }
         },
-        series: [{//Each number is an average calculated per wave from all data sources
-            name: 'Melee',
-            data: [178,177,156,134,173,122,138,157,172,164,171,157,107,187,185,166,147,171,122,147,141,131,176,137,100,139,144,234,100,168]
-        }, {
-            name: 'Charged Melee',
-            data: [32,21,20,33,36,30,34,27,34,34,17,27,33,39,31,34,31,37,32,29,20,30,26,32,26,36,26,36,25,27]
-        }, {
-            name: 'Range',
-            data: [58,89,84,52,94,132,113,78,78,67,89,78,133,114,120,71,82,67,79,69,78,125,118,102,87,131,76,73,129,68]
-        }, {
-            name: 'Charged Range',
-            data: [53,65,73,63,80,54,67,64,74,56,49,45,68,57,42,56,61,53,52,59,54,53,49,62,47,55,60,60,65,66]
-        }
-        ]
+        series: [{
+            type: 'pie',
+            name: 'Seconds spent',
+            data: [
+                ['Walking',   160],//Average Elapsed time - strafing time
+                ['Strafing',   90]//Average Strafe Time
+            ]
+        }]
     });
-});
+});   
+   //Display Chart #5
+
    //End Display Charts
  });
 });
